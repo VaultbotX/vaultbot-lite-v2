c97efa6268e7c72bad836d05e405f379d7ed64ad
Trey Brittain <treybrittain@gmail.com>
2023-04-30 16:12:11 -0500

    Add add and remove commands to coordinate Spotify and Database operations

---
+++ internal/commands/add.go
@@ -0,0 +1,15 @@
+package commands
+
+func AddTrack(trackId string) error {
+	// 1. Attempt to get the track from Spotify
+
+	// 2. If exists, pull the artists and song features
+
+	// 3. Save to database(s)
+
+	// 4. Add to playlist
+
+	// If failure during step 4, rollback database(s)
+
+	return nil
+}
---
+++ internal/commands/remove.go
@@ -0,0 +1,9 @@
+package commands
+
+func RemoveTracks(trackIds []string) error {
+	// 1. Remove tracks from playlist
+
+	// 2. Remove track from database(s) - practically speaking, this will just be a redis cache
+
+	return nil
+}
