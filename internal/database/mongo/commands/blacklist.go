e41b20ae18baa3e863dcbc0059845a7f9927d937
Trey Brittain <treybrittain@gmail.com>
2023-05-14 23:57:36 -0500

    Add functions for blacklisting, unblacklisting, and checking if an item is blacklisted for tracks, artists, and genres

--- internal/database/mongo/commands/blacklist.go
+++ internal/database/mongo/commands/blacklist.go
@@ -5,11 +5,21 @@ import (
 	mg "github.com/vaultbotx/vaultbot-lite/internal/database/mongo"
 	"github.com/vaultbotx/vaultbot-lite/internal/database/mongo/types"
 	internaltypes "github.com/vaultbotx/vaultbot-lite/internal/types"
-	"github.com/zmb3/spotify/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"time"
 )
 
-func BlacklistTrack(ctx context.Context, trackId spotify.ID, userFields *internaltypes.UserFields, time time.Time) error {
+type BlacklistType int
+
+const (
+	Track BlacklistType = iota
+	Artist
+	Genre
+)
+
+func Blacklist(ctx context.Context, blacklistType BlacklistType, id string,
+	userFields *internaltypes.UserFields, time time.Time) error {
 	instance, err := mg.GetMongoClient(ctx)
 	if err != nil {
 		return err
@@ -17,15 +27,35 @@ func BlacklistTrack(ctx context.Context, trackId spotify.ID, userFields *interna
 	defer instance.Disconnect(ctx)
 
 	collection := instance.Database(mg.DatabaseName).Collection(mg.BlacklistCollection)
-	newBlacklistedTrack := types.BlacklistedTrack{
-		TrackId:           trackId.String(),
-		BlockedById:       userFields.UserId,
-		BlockedByUsername: userFields.Username,
-		GuildId:           userFields.GuildId,
-		Timestamp:         time.Unix(),
+	var blacklistedItem interface{}
+	switch blacklistType {
+	case Track:
+		blacklistedItem = types.BlacklistedTrack{
+			TrackId:           id,
+			BlockedById:       userFields.UserId,
+			BlockedByUsername: userFields.Username,
+			GuildId:           userFields.GuildId,
+			Timestamp:         time.Unix(),
+		}
+	case Artist:
+		blacklistedItem = types.BlacklistedArtist{
+			ArtistId:          id,
+			BlockedById:       userFields.UserId,
+			BlockedByUsername: userFields.Username,
+			GuildId:           userFields.GuildId,
+			Timestamp:         time.Unix(),
+		}
+	case Genre:
+		blacklistedItem = types.BlacklistedGenre{
+			GenreName:         id,
+			BlockedById:       userFields.UserId,
+			BlockedByUsername: userFields.Username,
+			GuildId:           userFields.GuildId,
+			Timestamp:         time.Unix(),
+		}
 	}
 
-	_, err = collection.InsertOne(ctx, newBlacklistedTrack)
+	_, err = collection.InsertOne(ctx, blacklistedItem)
 	if err != nil {
 		return err
 	}
@@ -33,7 +63,8 @@ func BlacklistTrack(ctx context.Context, trackId spotify.ID, userFields *interna
 	return nil
 }
 
-func BlacklistArtist(ctx context.Context, artistId spotify.ID, userFields *internaltypes.UserFields, time time.Time) error {
+func Unblacklist(ctx context.Context, blacklistType BlacklistType, id string,
+	userFields *internaltypes.UserFields) error {
 	instance, err := mg.GetMongoClient(ctx)
 	if err != nil {
 		return err
@@ -41,42 +72,57 @@ func BlacklistArtist(ctx context.Context, artistId spotify.ID, userFields *inter
 	defer instance.Disconnect(ctx)
 
 	collection := instance.Database(mg.DatabaseName).Collection(mg.BlacklistCollection)
-	newBlacklistedArtist := types.BlacklistedArtist{
-		ArtistId:          artistId.String(),
-		BlockedById:       userFields.UserId,
-		BlockedByUsername: userFields.Username,
-		GuildId:           userFields.GuildId,
-		Timestamp:         time.Unix(),
+
+	var filter bson.M
+	switch blacklistType {
+	case Track:
+		filter = bson.M{"trackId": id}
+	case Artist:
+		filter = bson.M{"artistId": id}
+	case Genre:
+		filter = bson.M{"genreName": id}
 	}
 
-	_, err = collection.InsertOne(ctx, newBlacklistedArtist)
+	result, err := collection.DeleteOne(ctx, filter)
 	if err != nil {
 		return err
+	}
+
+	if result.DeletedCount == 0 {
+		return types.ErrNoDocuments
 	}
 
 	return nil
 }
 
-func BlacklistGenre(ctx context.Context, genreName string, userFields *internaltypes.UserFields, time time.Time) error {
+func CheckBlacklistItem(ctx context.Context, blacklistType BlacklistType, id string) (bool, error) {
 	instance, err := mg.GetMongoClient(ctx)
 	if err != nil {
-		return err
+		return false, err
 	}
 	defer instance.Disconnect(ctx)
 
 	collection := instance.Database(mg.DatabaseName).Collection(mg.BlacklistCollection)
-	newBlacklistedGenre := types.BlacklistedGenre{
-		GenreName:         genreName,
-		BlockedById:       userFields.UserId,
-		BlockedByUsername: userFields.Username,
-		GuildId:           userFields.GuildId,
-		Timestamp:         time.Unix(),
+
+	var filter bson.M
+	switch blacklistType {
+	case Track:
+		filter = bson.M{"trackId": id}
+	case Artist:
+		filter = bson.M{"artistId": id}
+	case Genre:
+		filter = bson.M{"genreName": id}
 	}
 
-	_, err = collection.InsertOne(ctx, newBlacklistedGenre)
+	result := collection.FindOne(ctx, filter)
+	err = result.Err()
 	if err != nil {
-		return err
+		if err == mongo.ErrNoDocuments {
+			return false, nil
+		}
+
+		return false, err
 	}
 
-	return nil
+	return true, nil
 }
