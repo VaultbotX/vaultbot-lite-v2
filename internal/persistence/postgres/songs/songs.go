d8ba662a37ea834159722be4df565dbccf658804
Trey Brittain <treybrittain@gmail.com>
2025-03-02 10:46:10 -0600

    Use float32 for props on Song struct

--- internal/persistence/postgres/songs/songs.go
+++ internal/persistence/postgres/songs/songs.go
@@ -2,6 +2,7 @@ package songs
 
 import (
 	"github.com/jmoiron/sqlx"
+	"github.com/zmb3/spotify/v2"
 	"time"
 )
 
@@ -11,48 +12,49 @@ type Song struct {
 	Name             string    `db:"name"`
 	ReleaseDate      time.Time `db:"release_date"`
 	SpotifyAlbumId   string    `db:"spotify_album_id"`
-	Acousticness     float64   `db:"acousticness"`
-	Danceability     float64   `db:"danceability"`
+	Acousticness     float32   `db:"acousticness"`
+	Danceability     float32   `db:"danceability"`
 	DurationMs       int       `db:"duration_ms"`
-	Energy           float64   `db:"energy"`
-	Instrumentalness float64   `db:"instrumentalness"`
+	Energy           float32   `db:"energy"`
+	Instrumentalness float32   `db:"instrumentalness"`
 	Key              int       `db:"key"`
-	Liveness         float64   `db:"liveness"`
-	Loudness         float64   `db:"loudness"`
+	Liveness         float32   `db:"liveness"`
+	Loudness         float32   `db:"loudness"`
 	Mode             int       `db:"mode"`
-	Speechiness      float64   `db:"speechiness"`
-	Tempo            float64   `db:"tempo"`
+	Speechiness      float32   `db:"speechiness"`
+	Tempo            float32   `db:"tempo"`
 	TimeSignature    int       `db:"time_signature"`
-	Valence          float64   `db:"valence"`
+	Valence          float32   `db:"valence"`
 	CreatedAt        time.Time `db:"created_at"`
 }
 
 var DefaultSong = Song{}
 
 // AddSong adds a song to the database
-func AddSong(db *sqlx.DB, spotifyId string, name string, releaseDate time.Time, spotifyAlbumId string, acousticness float64, danceability float64, durationMs int, energy float64, instrumentalness float64, key int, liveness float64, loudness float64, mode int, speechiness float64, tempo float64, timeSignature int, valence float64) (Song, error) {
+func AddSong(db *sqlx.DB, track *spotify.FullTrack, audioFeatures []*spotify.AudioFeatures) (Song, error) {
+	firstAudioFeature := audioFeatures[0]
 	row, err := db.NamedExec(`
 		INSERT INTO songs (spotify_id, name, release_date, spotify_album_id, acousticness, danceability, duration_ms, energy, instrumentalness, key, liveness, loudness, mode, speechiness, tempo, time_signature, valence) 
 		VALUES (:spotify_id, :name, :release_date, :spotify_album_id, :acousticness, :danceability, :duration_ms, :energy, :instrumentalness, :key, :liveness, :loudness, :mode, :speechiness, :tempo, :time_signature, :valence)
 		ON CONFLICT (spotify_id) DO NOTHING
 	`, map[string]interface{}{
-		"spotify_id":       spotifyId,
-		"name":             name,
-		"release_date":     releaseDate,
-		"spotify_album_id": spotifyAlbumId,
-		"acousticness":     acousticness,
-		"danceability":     danceability,
-		"duration_ms":      durationMs,
-		"energy":           energy,
-		"instrumentalness": instrumentalness,
-		"key":              key,
-		"liveness":         liveness,
-		"loudness":         loudness,
-		"mode":             mode,
-		"speechiness":      speechiness,
-		"tempo":            tempo,
-		"time_signature":   timeSignature,
-		"valence":          valence,
+		"spotify_id":       track.ID.String(),
+		"name":             track.Name,
+		"release_date":     track.Album.ReleaseDate,
+		"spotify_album_id": track.Album.ID.String(),
+		"acousticness":     firstAudioFeature.Acousticness,
+		"danceability":     firstAudioFeature.Danceability,
+		"duration_ms":      firstAudioFeature.Duration,
+		"energy":           firstAudioFeature.Energy,
+		"instrumentalness": firstAudioFeature.Instrumentalness,
+		"key":              firstAudioFeature.Key,
+		"liveness":         firstAudioFeature.Liveness,
+		"loudness":         firstAudioFeature.Loudness,
+		"mode":             firstAudioFeature.Mode,
+		"speechiness":      firstAudioFeature.Speechiness,
+		"tempo":            firstAudioFeature.Tempo,
+		"time_signature":   firstAudioFeature.TimeSignature,
+		"valence":          firstAudioFeature.Valence,
 	})
 
 	if err != nil {
@@ -66,23 +68,23 @@ func AddSong(db *sqlx.DB, spotifyId string, name string, releaseDate time.Time, 
 
 	return Song{
 		Id:               int(id),
-		SpotifyId:        spotifyId,
-		Name:             name,
-		ReleaseDate:      releaseDate,
-		SpotifyAlbumId:   spotifyAlbumId,
-		Acousticness:     acousticness,
-		Danceability:     danceability,
-		DurationMs:       durationMs,
-		Energy:           energy,
-		Instrumentalness: instrumentalness,
-		Key:              key,
-		Liveness:         liveness,
-		Loudness:         loudness,
-		Mode:             mode,
-		Speechiness:      speechiness,
-		Tempo:            tempo,
-		TimeSignature:    timeSignature,
-		Valence:          valence,
+		SpotifyId:        track.ID.String(),
+		Name:             track.Name,
+		ReleaseDate:      track.Album.ReleaseDateTime(),
+		SpotifyAlbumId:   track.Album.ID.String(),
+		Acousticness:     firstAudioFeature.Acousticness,
+		Danceability:     firstAudioFeature.Danceability,
+		DurationMs:       int(firstAudioFeature.Duration),
+		Energy:           firstAudioFeature.Energy,
+		Instrumentalness: firstAudioFeature.Instrumentalness,
+		Key:              int(firstAudioFeature.Key),
+		Liveness:         firstAudioFeature.Liveness,
+		Loudness:         firstAudioFeature.Loudness,
+		Mode:             int(firstAudioFeature.Mode),
+		Speechiness:      firstAudioFeature.Speechiness,
+		Tempo:            firstAudioFeature.Tempo,
+		TimeSignature:    int(firstAudioFeature.TimeSignature),
+		Valence:          firstAudioFeature.Valence,
 		CreatedAt:        time.Now(),
 	}, nil
 }
