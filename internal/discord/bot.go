cd290f5f764999592eebcfc77d8b9ca1ff6948e4
Trey Brittain <treybrittain@gmail.com>
2025-03-05 21:17:59 -0600

    Remove invalid gateway event handlers

--- internal/discord/bot.go
+++ internal/discord/bot.go
@@ -41,16 +41,8 @@ func Run() {
 		log.Infof("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
 	})
 
-	s.AddHandler(func(s *discordgo.Session, e *discordgo.APIErrorMessage) {
-		log.Errorf("Discord API error: %v", e.Message)
-	})
-
-	s.AddHandler(func(s *discordgo.Session, e *discordgo.RESTError) {
-		log.Errorf("Discord REST error: %v", e.Message)
-	})
-
-	s.AddHandler(func(s *discordgo.Session, e *discordgo.RateLimitError) {
-		log.Errorf("Discord rate limit error: %v", e.Message)
+	s.AddHandler(func(s *discordgo.Session, e *discordgo.Resumed) {
+		log.Info("Resumed session")
 	})
 
 	startBackgroundTasks()
