cd8af0879bf1eb01ae66e80a19f804c497acc732
Trey Brittain <treybrittain@gmail.com>
2025-03-08 18:16:34 -0600

    Reuse sqlx DB pool

--- internal/persistence/postgres/conn.go
+++ internal/persistence/postgres/conn.go
@@ -4,9 +4,24 @@ import (
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"os"
+	"sync"
+)
+
+var (
+	// db is the global database connection. It is perfectly fine for a sqlx.DB to be global since it
+	// simply represents a pool of connections rather than a single connection.
+	db *sqlx.DB
+	mu sync.Mutex
 )
 
 func NewPostgresConnection() (*sqlx.DB, error) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	if db != nil {
+		return db, nil
+	}
+
 	host := os.Getenv("POSTGRES_HOST")
 	user := os.Getenv("POSTGRES_USER")
 	password := os.Getenv("POSTGRES_PASSWORD")
@@ -19,10 +34,12 @@ func NewPostgresConnection() (*sqlx.DB, error) {
 
 	dsn := "host=" + host + " user=" + user + " password=" + password + " dbname=vaultbot sslmode=disable"
 
-	db, err := sqlx.Open("postgres", dsn)
+	newDb, err := sqlx.Open("postgres", dsn)
 	if err != nil {
 		return nil, err
 	}
+
+	db = newDb
 
 	return db, nil
 }
