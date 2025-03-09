b0f8575447fa9d3b4a739000ac6138eac8f5086f
Trey Brittain <treybrittain@gmail.com>
2025-03-09 14:09:04 -0500

    TF remote backend config + init script + build out terraform.yml with destructive changes validation step

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,15 +1,28 @@
 # This backend must be manually bootstrapped in DigitalOcean Spaces.
 # Configuration script for this in particular TBD but this will be necessary
-# to run once to create the initial state file if starting from scratch.
+# to run once to create the initial remote state file if starting from scratch.
+
+# Most values empty since we will pass these in via CLI (partial config)
+# See tf_init.ps1
 terraform {
+  required_version = ">= 1.6.3"
+
   backend "s3" {
-    bucket         = "terraform-state-${var.environment}"
-    key            = "${var.environment}/terraform.tfstate"
+    endpoints = {
+      s3 = "https://nyc3.digitaloceanspaces.com"
+    }
+
+    bucket         = ""
+    key            = ""
     region         = "nyc3"
-    endpoint       = "https://nyc3.digitaloceanspaces.com"
-    access_key     = var.do_spaces_access_key
-    secret_key     = var.do_spaces_secret_key
-    skip_region_validation      = true
+    access_key     = ""
+    secret_key     = ""
+
+    # These may be AWS specific, so disabling them for DO
     skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_s3_checksum            = true
   }
 }
