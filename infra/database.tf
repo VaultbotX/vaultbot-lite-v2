16cde842f732ecd64f09689e84a8294352abf13b
Trey Brittain <treybrittain@gmail.com>
2025-03-15 00:52:35 -0500

    Remove VPC for time being

--- infra/database.tf
+++ infra/database.tf
@@ -5,7 +5,6 @@ resource "digitalocean_database_cluster" "vaultbot_postgres_cluster" {
   size       = "db-s-1vcpu-1gb"
   region     = var.do_region
   node_count = 1
-  private_network_uuid = digitalocean_vpc.vaultbot_vpc.id
 
   maintenance_window {
     day  = "monday"
@@ -15,5 +14,5 @@ resource "digitalocean_database_cluster" "vaultbot_postgres_cluster" {
 
 resource "digitalocean_database_firewall" "vaultbot-postgres-firewall" {
   cluster_id = digitalocean_database_cluster.vaultbot_postgres_cluster.id
-  # no inbound rules configured here. will manage this via the UI
+  # no inbound rules configured here. will try to manage this via the UI
 }
