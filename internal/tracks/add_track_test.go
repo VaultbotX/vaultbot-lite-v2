4925f4d328a12846b976719424c95695b7c3b600
Trey Brittain <treybrittain@gmail.com>
2025-03-16 11:39:43 -0500

    Add mock repositories and tests for track and artist blacklisting

--- internal/tracks/add_track_test.go
+++ internal/tracks/add_track_test.go
@@ -53,18 +53,29 @@ func TestAddTrack_ShortCircuits_IfTrackIsInPlaylist(t *testing.T) {
 	}
 }
 
-type MockSpotifyTrackRepo struct {
+type mockSpotifyTrackRepo struct {
 	getTrackResponse *spotify.FullTrack
 }
 
-func (m *MockSpotifyTrackRepo) GetTrack(_ spotify.ID, trackChan chan<- *spotify.FullTrack, _ context.Context) error {
+func (m *mockSpotifyTrackRepo) GetTrack(_ spotify.ID, trackChan chan<- *spotify.FullTrack, _ context.Context) error {
 	trackChan <- m.getTrackResponse
+	return nil
+}
+
+type mockSpotifyArtistRepo struct {
+	getArtistsResponse []*spotify.FullArtist
+}
+
+func (m *mockSpotifyArtistRepo) GetArtists(_ []spotify.ID, artistChan chan<- *spotify.FullArtist, _ context.Context) error {
+	for _, artist := range m.getArtistsResponse {
+		artistChan <- artist
+	}
 	return nil
 }
 
 func TestAddTrack_ShortCircuits_IfTrackNotFound(t *testing.T) {
 	// Arrange
-	spotifyTrackService := domain.NewSpotifyTrackService(&MockSpotifyTrackRepo{
+	spotifyTrackService := domain.NewSpotifyTrackService(&mockSpotifyTrackRepo{
 		getTrackResponse: nil,
 	})
 	input := &AddTrackInput{
@@ -78,5 +89,117 @@ func TestAddTrack_ShortCircuits_IfTrackNotFound(t *testing.T) {
 	// Assert
 	if !errors.Is(err, domain.ErrNoTrackExists) {
 		t.Errorf("Expected %v, got %v", domain.ErrNoTrackExists, err)
+	}
+}
+
+type blacklistedItem struct {
+	blacklistType domain.EntityType
+	id            string
+}
+
+type mockBlacklistRepo struct {
+	blacklistedItems []blacklistedItem
+}
+
+func (m *mockBlacklistRepo) AddToBlacklist(_ context.Context, _ domain.EntityType, _ string,
+	_ *domain.UserFields) error {
+	return nil
+}
+
+func (m *mockBlacklistRepo) RemoveFromBlacklist(_ context.Context, _ domain.EntityType, _ string) error {
+	return nil
+}
+
+func (m *mockBlacklistRepo) CheckBlacklistItem(_ context.Context, blacklistType domain.EntityType, id string) (bool, error) {
+	for _, item := range m.blacklistedItems {
+		if item.blacklistType == blacklistType && item.id == id {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+func TestAddTrack_ShortCircuits_IfArtistBlacklisted(t *testing.T) {
+	// Arrange
+	spotifyTrackService := domain.NewSpotifyTrackService(&mockSpotifyTrackRepo{
+		getTrackResponse: &spotify.FullTrack{
+			SimpleTrack: spotify.SimpleTrack{
+				Artists: []spotify.SimpleArtist{
+					{
+						ID:   "blacklistedartistid",
+						Name: "Blacklisted Artist",
+					},
+				},
+			},
+		},
+	})
+	spotifyArtistService := domain.NewSpotifyArtistService(&mockSpotifyArtistRepo{
+		getArtistsResponse: []*spotify.FullArtist{
+			{
+				SimpleArtist: spotify.SimpleArtist{
+					ID:   "blacklistedartistid",
+					Name: "Blacklisted Artist",
+				},
+			},
+		},
+	})
+	blacklistService := domain.NewBlacklistService(&mockBlacklistRepo{
+		blacklistedItems: []blacklistedItem{
+			{
+				blacklistType: domain.Artist,
+				id:            "blacklistedartistid",
+			},
+		},
+	})
+	input := &AddTrackInput{
+		TrackId:          "validtrackid3",
+		SpTrackService:   spotifyTrackService,
+		SpArtistService:  spotifyArtistService,
+		BlacklistService: blacklistService,
+	}
+
+	// Act
+	_, err := AddTrack(input)
+
+	// Assert
+	var eab *domain.ErrArtistBlacklisted
+	ok := errors.As(err, &eab)
+	if !ok {
+		t.Errorf("Expected %v, got %v", &domain.ErrArtistBlacklisted{}, err)
+	}
+}
+
+func TestAddTrack_ShortCircuits_IfTrackBlacklisted(t *testing.T) {
+	// Arrange
+	spotifyTrackService := domain.NewSpotifyTrackService(&mockSpotifyTrackRepo{
+		getTrackResponse: &spotify.FullTrack{
+			SimpleTrack: spotify.SimpleTrack{
+				ID: "blacklistedtrackid",
+			},
+		},
+	})
+	blacklistService := domain.NewBlacklistService(&mockBlacklistRepo{
+		blacklistedItems: []blacklistedItem{
+			{
+				blacklistType: domain.Track,
+				id:            "blacklistedtrackid",
+			},
+		},
+	})
+	input := &AddTrackInput{
+		TrackId:          "validtrackid4",
+		SpTrackService:   spotifyTrackService,
+		BlacklistService: blacklistService,
+	}
+
+	// Act
+	_, err := AddTrack(input)
+
+	// Assert
+	var etb *domain.ErrTrackBlacklisted
+	ok := errors.As(err, &etb)
+	if !ok {
+		t.Errorf("Expected %v, got %v", &domain.ErrTrackBlacklisted{}, err)
 	}
 }
