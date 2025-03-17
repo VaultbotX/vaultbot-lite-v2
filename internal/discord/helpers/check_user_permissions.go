373283a8bd5862fbc3d1c0de48cff7d5de107ff4
Trey Brittain <treybrittain@gmail.com>
2025-03-17 13:41:03 -0500

    Refactor CheckUserPermissions to retrieve administrator user ID at runtime and handle missing environment variable error

--- internal/discord/helpers/check_user_permissions.go
+++ internal/discord/helpers/check_user_permissions.go
@@ -1,23 +1,11 @@
 package helpers
 
 import (
+	"errors"
 	"github.com/bwmarrin/discordgo"
 	"github.com/vaultbotx/vaultbot-lite/internal/domain"
 	"os"
 )
-
-var (
-	administratorUserId string
-)
-
-func init() {
-	id, exists := os.LookupEnv("DISCORD_ADMINISTRATOR_USER_ID")
-	if !exists {
-		panic("DISCORD_ADMINISTRATOR_USER_ID environment variable is required")
-	}
-
-	administratorUserId = id
-}
 
 func CheckUserPermissions(s *discordgo.Session, i *discordgo.InteractionCreate) error {
 	perms, err := s.State.UserChannelPermissions(i.Member.User.ID, i.ChannelID)
@@ -30,6 +18,11 @@ func CheckUserPermissions(s *discordgo.Session, i *discordgo.InteractionCreate) 
 	}
 
 	// Secondary check to limit commands to only the bot owner for now
+	administratorUserId, exists := os.LookupEnv("DISCORD_ADMINISTRATOR_USER_ID")
+	if !exists {
+		return errors.New("DISCORD_ADMINISTRATOR_USER_ID not set")
+	}
+
 	if i.Member.User.ID != administratorUserId {
 		return domain.ErrUnauthorized
 	}
