32c881c9c6f5eb856de9b1ad5abdba7a292c1bfd
Trey Brittain <treybrittain@gmail.com>
2025-03-17 13:31:59 -0500

    Add environment variable for Discord administrator user ID and update permission checks

--- internal/discord/helpers/check_user_permissions.go
+++ internal/discord/helpers/check_user_permissions.go
@@ -3,7 +3,21 @@ package helpers
 import (
 	"github.com/bwmarrin/discordgo"
 	"github.com/vaultbotx/vaultbot-lite/internal/domain"
+	"os"
 )
+
+var (
+	administratorUserId string
+)
+
+func init() {
+	id, exists := os.LookupEnv("DISCORD_ADMINISTRATOR_USER_ID")
+	if !exists {
+		panic("DISCORD_ADMINISTRATOR_USER_ID environment variable is required")
+	}
+
+	administratorUserId = id
+}
 
 func CheckUserPermissions(s *discordgo.Session, i *discordgo.InteractionCreate) error {
 	perms, err := s.State.UserChannelPermissions(i.Member.User.ID, i.ChannelID)
@@ -12,6 +26,11 @@ func CheckUserPermissions(s *discordgo.Session, i *discordgo.InteractionCreate) 
 	}
 
 	if perms&discordgo.PermissionAdministrator == 0 {
+		return domain.ErrUnauthorized
+	}
+
+	// Secondary check to limit commands to only the bot owner for now
+	if i.Member.User.ID != administratorUserId {
 		return domain.ErrUnauthorized
 	}
 
