ad08dbd484234d3b6baa3271bebc1b4f25faf885
Trey Brittain <treybrittain@gmail.com>
2025-04-06 14:51:40 -0500

    Fix username formatting in logging_meta.go to include '#' separator

--- internal/utils/logging_meta.go
+++ internal/utils/logging_meta.go
@@ -11,14 +11,14 @@ func GetFieldsFromInteraction(i *discordgo.InteractionCreate) logrus.Fields {
 	if isDirectMessage {
 		return logrus.Fields{
 			"userId":          i.User.ID,
-			"username":        i.User.Username + i.User.Discriminator,
+			"username":        i.User.Username + "#" + i.User.Discriminator,
 			"isDirectMessage": isDirectMessage,
 		}
 	}
 
 	return logrus.Fields{
 		"userId":          i.Member.User.ID,
-		"username":        i.Member.User.Username + i.Member.User.Discriminator,
+		"username":        i.Member.User.Username + "#" + i.Member.User.Discriminator,
 		"guildId":         i.GuildID,
 		"isDirectMessage": isDirectMessage,
 	}
