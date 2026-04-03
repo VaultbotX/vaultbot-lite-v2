1e6bfbb4e5acb123da72b9ab78e0108f99d241f2
Trey Brittain <treybrittain@gmail.com>
2026-04-03 10:55:10 -0500

    refactor: remove unused PostgreSQL cluster reference from project resource list

--- infra/project.tf
+++ infra/project.tf
@@ -2,7 +2,6 @@ resource "digitalocean_project" "vaultbot" {
   name        = "Vaultbot-${var.environment}"
   description = "Vaultbot Project for ${var.environment} environment"
   resources = [
-    digitalocean_database_cluster.vaultbot_postgres_cluster.urn,
     digitalocean_app.vaultbot_app.urn,
     digitalocean_app.vaultbot_migration_runner.urn
   ]
